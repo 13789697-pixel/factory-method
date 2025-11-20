abbaa0bdcf2d56b22bc3d82bfdb7d58166ca7cd9
Серьоженко Дмитро <13789697@gsuite.duit.edu.ua>
2025-11-20 16:06:27 +0200

    Add files via upload

---
+++ factory-method.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+//клас продукту//
+class IMessage {
+public:
+    virtual void show() const = 0;
+    virtual ~IMessage() = default;
+};
+// конкретні продукти//
+class HelloMessage : public IMessage {
+public:
+    void show() const override {
+        cout << "Hello" << endl;
+    }
+};
+class GoodbyeMessage : public IMessage {
+public:
+    void show() const override {
+        cout << "Goodbye" << endl;
+    }
+};
+// Фабричний метод //
+IMessage* createMessage(const string& type) {
+    if (type == "hello") {
+        return new HelloMessage();
+    }
+    else if (type == "goodbye") {
+        return new GoodbyeMessage();
+    }
+    else {
+        return nullptr;
+    }
+}
+int main() {
+    IMessage* msg1 = createMessage("hello");
+    IMessage* msg2 = createMessage("goodbye");
+
+    if (msg1) msg1->show();
+    if (msg2) msg2->show();
+    return 0;
+}
